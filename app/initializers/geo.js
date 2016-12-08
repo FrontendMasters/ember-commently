c07ac5f5a0a90279767602452b74d552c7c3d980
Mike North <michael.l.north@gmail.com>
2016-12-08 15:49:22 -0600

    Request user agent, and shoebox

--- app/initializers/geo.js
+++ app/initializers/geo.js
@@ -1,19 +1,19 @@
 export function initialize(app) {
-    if (typeof FastBoot === 'undefined' && navigator.geolocation) {
-        const geo = navigator.geolocation;
-        app.deferReadiness();
-        geo.getCurrentPosition((pos) => {
-            let pt = pos.coords;
-            let loc = {
-                lat: pt.latitude,
-                lng: pt.longitude
-            };
-            app.register('data:location', loc, {
-                instantiate: false
-            });
-            app.advanceReadiness();
-        });
-    }
+    // if (typeof FastBoot === 'undefined' && navigator.geolocation) {
+    //     const geo = navigator.geolocation;
+    //     app.deferReadiness();
+    //     geo.getCurrentPosition((pos) => {
+    //         let pt = pos.coords;
+    //         let loc = {
+    //             lat: pt.latitude,
+    //             lng: pt.longitude
+    //         };
+    //         app.register('data:location', loc, {
+    //             instantiate: false
+    //         });
+    //         app.advanceReadiness();
+    //     });
+    // }
 }
 
 export default {
--- app/routes/application.js
+++ app/routes/application.js
@@ -19,6 +19,8 @@ export default Route.extend(ApplicationRouteMixin, {
     setupController(controller) {
         this._super(...arguments);
         let locObj = getOwner(this).lookup('data:location');
+        let reqObj = getOwner(this).lookup('data:request');
         controller.set('loc', locObj);
+        controller.set('req', reqObj);
     }
 });
