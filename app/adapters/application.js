cd3c2d040a227017f34c606605211680936c5f02
Mike North <michael.l.north@gmail.com>
2016-12-07 22:45:48 -0600

    simple-auth ember-data adapter

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -1,10 +1,12 @@
 import DS from 'ember-data';
+import DataAdapterMixin from 'ember-simple-auth/mixins/data-adapter-mixin';
 import config from '../config/environment';
 
 const { JSONAPIAdapter } = DS;
 const { host, namespace } = config.DS;
 
-export default JSONAPIAdapter.extend({
+export default JSONAPIAdapter.extend(DataAdapterMixin, {
+    authorizer: 'authorizer:oauth2',
     host,
     namespace
 });
