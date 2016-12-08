69dc90e2721728ae8b9ebcf9d98aa2ad2c5ab24e
Mike North <michael.l.north@gmail.com>
2016-12-08 11:51:12 -0600

    Exercise 2

--- app/routes/application.js
+++ app/routes/application.js
@@ -1,8 +1,9 @@
+// app/routes/application.js
 import Ember from 'ember';
 import ApplicationRouteMixin from 'ember-simple-auth/mixins/application-route-mixin';
 import { default as math, PI } from 'math';
 
-const { Route, inject } = Ember;
+const { Route, inject, getOwner } = Ember;
 
 export default Route.extend(ApplicationRouteMixin, {
     session: inject.service(),
@@ -14,5 +15,10 @@ export default Route.extend(ApplicationRouteMixin, {
     beforeModel() {
         this._super(...arguments);
         console.log(math, PI);
+    },
+    setupController(controller) {
+        this._super(...arguments);
+        let locObj = getOwner(this).lookup('data:location');
+        controller.set('loc', locObj);
     }
 });
