6f2704a9e821c1e5504188348c2eb39a89e6f0cf
Mike North <michael.l.north@gmail.com>
2017-04-04 19:49:07 -0700

    Disable geolocation stuff

--- app/initializers/geo.js
+++ app/initializers/geo.js
@@ -1,21 +1,21 @@
 export function initialize(app) {
-  if (typeof FastBoot === 'undefined') {
-    let geo = navigator.geolocation;
-    if (geo) { // Skip this part for PhantomJS
-      app.deferReadiness();
-      geo.getCurrentPosition((pos) => {
-        let pt = pos.coords;
-        let loc = {
-          lat: pt.latitude,
-          lng: pt.longitude
-        };
-        app.register('data:location', loc, {
-          instantiate: false
-        });
-        app.advanceReadiness();
-      });
-    }
-  }
+  // if (typeof FastBoot === 'undefined') {
+  //   let geo = navigator.geolocation;
+  //   if (geo) { // Skip this part for PhantomJS
+  //     app.deferReadiness();
+  //     geo.getCurrentPosition((pos) => {
+  //       let pt = pos.coords;
+  //       let loc = {
+  //         lat: pt.latitude,
+  //         lng: pt.longitude
+  //       };
+  //       app.register('data:location', loc, {
+  //         instantiate: false
+  //       });
+  //       app.advanceReadiness();
+  //     });
+  //   }
+  // }
 }
 
 export default {
