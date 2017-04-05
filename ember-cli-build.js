6062acc724a43dbd059c7d1e91a524a0b1da20d1
Mike North <michael.l.north@gmail.com>
2017-04-04 19:28:25 -0700

    Challenge #3 solution

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -1,23 +1,32 @@
 /* eslint-env node */
 const EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
-module.exports = function(defaults) {
+const Filter = require('broccoli-filter');
+
+module.exports = function (defaults) {
   var app = new EmberApp(defaults, {
     // Add options here
   });
 
-  // Use `app.import` to add additional libraries to the generated
-  // output files.
-  //
-  // If you need to use different assets in different
-  // environments, specify an object as the first parameter. That
-  // object's keys should be the environment name and the values
-  // should be the asset to use in that environment.
-  //
-  // If the library that you are including contains AMD or ES6
-  // modules that you would like to import into your application
-  // please specify an object with the list of modules as keys
-  // along with the exports of each module as its value.
+  function MyFilter(inputNode) {
+    Filter.call(this, inputNode);
+  }
 
-  return app.toTree();
+  MyFilter.prototype = Object.create(Filter.prototype);
+
+  MyFilter.prototype.processString = function(existingString) {
+  var prepend = `/**
+ * vendor.js
+ *
+ * (c) 2017 🦄🦄🦄🔫🌈🍺🍺 All Rights Reserved
+ * generated at: ${new Date().toISOString()}
+ */
+`;
+    return prepend + existingString;
+  };
+
+  MyFilter.prototype.extensions = ['js'];
+  MyFilter.prototype.targetExtension = 'js';
+
+  return new MyFilter(app.toTree());
 };
