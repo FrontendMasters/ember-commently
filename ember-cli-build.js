9d7e6c8a4b4e7fabf440ea3d5b65dfb68da7b1ad
Mike North <michael.l.north@gmail.com>
2017-04-04 23:43:48 -0700

    Challenge #16 solution

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -1,32 +1,10 @@
 /* eslint-env node */
 const EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
-const Filter = require('broccoli-filter');
-
 module.exports = function (defaults) {
   var app = new EmberApp(defaults, {
     // Add options here
   });
 
-  function MyFilter(inputNode) {
-    Filter.call(this, inputNode);
-  }
-
-  MyFilter.prototype = Object.create(Filter.prototype);
-
-  MyFilter.prototype.processString = function(existingString) {
-  var prepend = `/**
- * vendor.js
- *
- * (c) 2017 🦄🦄🦄🔫🌈🍺🍺 All Rights Reserved
- * generated at: ${new Date().toISOString()}
- */
-`;
-    return prepend + existingString;
-  };
-
-  MyFilter.prototype.extensions = ['js'];
-  MyFilter.prototype.targetExtension = 'js';
-
-  return new MyFilter(app.toTree());
+  return app.toTree();
 };
